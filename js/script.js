75e5c887e754f606295d3860374a83821bb667c0
yzrm <140807906+yzrm@users.noreply.github.com>
2025-02-07 19:17:04 +0900

    iceCreamの画像を一部反転、アニメーションを追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
+//ローディング画面制御
+const loading = document.querySelector(".loading");
+
+//ページの読み込み完了時に処理を実行
+window.addEventListener("load", () => {
+   //3秒後にローディング画面を非表示にする
+  setTimeout(() => {
+    loading.classList.add("loaded");
+  }, 3000);
+});
+
+
+
 //　横スクロール制御
 const container = document.querySelector('.timeline');
 const slides = document.querySelectorAll('.timelineListContent');
